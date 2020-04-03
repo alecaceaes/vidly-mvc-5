0798838b12410ebc7dfae4d7ba248312fbe42633
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 14:48:35 +0800

    Solution - Section 4 Exercise - Add a form to create/edit a movie. (Also Added the Genres Migration, I forgot)

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -30,6 +30,34 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
@@ -62,10 +90,26 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
-        [Route("movies/release/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleasedDate(int year, int month)
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
-            return Content(year + "/" + month);
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
     }
 }
