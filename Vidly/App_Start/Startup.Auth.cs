26a24a3bcac701960595a18e317e5cc4291d59bc
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 19:14:41 +0800

    Social Logins

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace Vidly
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "1234",
+               appSecret: "1234");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
