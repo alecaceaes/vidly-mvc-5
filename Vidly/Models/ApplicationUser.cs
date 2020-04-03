ae99cc2bc9de63d627fb845b4d9f76340b01bee1
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 19:47:23 +0800

    Building the Simplest API

--- Vidly/Models/ApplicationUser.cs
+++ Vidly/Models/ApplicationUser.cs
@@ -24,23 +24,5 @@ namespace Vidly.Models
             // Add custom user claims here
             return userIdentity;
         }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<Movie> Movies { get; set; }
-        public DbSet<Genre> Genres { get; set; }
-        public DbSet<MembershipType> MembershipTypes { get; set; }
-
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+    }    
 }
