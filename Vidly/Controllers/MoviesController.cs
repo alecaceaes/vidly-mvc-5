779a60251e58481f436d7393aa90499c41ddf26c
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 12:13:57 +0800

    Convention-based Routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -19,21 +19,9 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
-
-        public ActionResult Edit(int id)
+        public ActionResult ByReleasedDate(int year, int month)
         {
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&soryBy{1}", pageIndex, sortBy));
+            return Content(year + "/" + month);
         }
     }
 }
