33ec0b356f66f6f4e1dc43cba70093a1e4339204
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 14:26:10 +0800

    Drop-down Lists

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -16,6 +16,8 @@ namespace Vidly.Models
 
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
