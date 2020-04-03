f74ee5401a30a81a909157751fdf7930a40420de
Alec Tecson <alec.tecson@nipa.ph>
2020-04-03 18:55:16 +0800

    Working with Roles

--- Vidly/Migrations/202004031034125_SeedUsers.cs
+++ Vidly/Migrations/202004031034125_SeedUsers.cs
@@ -9,7 +9,7 @@ namespace Vidly.Migrations
         {
             Sql(@"
             INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'2bee20db-b59e-47b2-8321-358797afaca8', N'admin@vidly.com', 0, N'AKxQKheDdt1WFs1MjIswwXQQKv7aLGHCQHTxtRTyYtuS84384TEYBIBVrb4NE8n3MA==', N'eceedf48-9def-4148-8704-56d17d6bd7b8', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
-            INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'd6c2fd3f-755e-47ef-bf83-6dc34d00853f', N'guest@vidly.com', 0, N'AKxQKheDdt1WFs1MjIswwXQQKv7aLGHCQHTxtRTyYtuS84384TEYBIBVrb4NE8n3MA==', N'bde59265-9adc-4fea-958f-9683e76b3226', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+            INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'd6c2fd3f-755e-47ef-bf83-6dc34d00853f', N'guest@vidly.com', 0, N'ANW1uEZT+x3AISiDe/IajksrYblQeJGRHuZ9h+QFtPNQmOIU6F01Vt0lbZf2Ed6Mxg==', N'663fe3e2-a523-41a4-8df3-c3972782aad0', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
             INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'4ec60cac-6536-41d0-841e-546e975a7aaa', N'CanManageMovie')
             INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'2bee20db-b59e-47b2-8321-358797afaca8', N'4ec60cac-6536-41d0-841e-546e975a7aaa')");
         }
